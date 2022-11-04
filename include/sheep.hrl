f8ba8eac7599e3df86ab3f54604d75c548b3d50c
Andrei Dziaineka <a_dziaineka@wargaming.net>
2022-11-04 12:37:01 +0000

    fix types

--- include/sheep.hrl
+++ include/sheep.hrl
@@ -3,24 +3,24 @@
 -record(sheep_request, {
     method :: binary(),
     path :: binary(),
-    headers = #{} :: maps:map(),
-    query = #{} :: maps:map(),
-    bindings = #{} :: maps:map(),
-    body = <<>> :: maps:map() | [maps:map()] | binary(),
+    headers = #{} :: map(),
+    query = #{} :: map(),
+    bindings = #{} :: map(),
+    body = <<>> :: map() | [map()] | binary(),
     peer :: {inet:ip_address(), inet:port_number()} | undefined
 }).
 -type sheep_request() :: #sheep_request{}.
 
 -record(sheep_response, {
     status_code = 500 :: http_code(),
-    headers = #{} :: maps:map(),
-    body = <<>> :: maps:map() | [maps:map()] | binary() | undefined
+    headers = #{} :: map(),
+    body = <<>> :: map() | [map()] | binary() | undefined
 }).
 -type sheep_response() :: #sheep_response{}.
 
 -record(sheep_options, {
-    encode_spec :: undefined | maps:map(),
-    decode_spec :: undefined | maps:map(),
-    method_spec :: undefined | maps:map()
+    encode_spec :: undefined | map(),
+    decode_spec :: undefined | map(),
+    method_spec :: undefined | map()
 }).
 -type sheep_options() :: #sheep_options{}.
